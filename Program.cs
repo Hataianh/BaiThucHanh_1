977f4b96e006c6d528a00b7c7b26c360649a1fe0
Hataianh <72591239+Hataianh@users.noreply.github.com>
2020-10-17 09:14:45 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -101,3 +101,115 @@ class Stack
             return s[top--];
     }
 }
+//Bai4
+class SoPhuc
+{
+    private double thuc, ao;
+    public SoPhuc()
+    {
+        thuc = 0;
+        ao = 0;
+    }
+    public SoPhuc(double thuc, double ao)
+    {
+        this.thuc = thuc;
+        this.ao = ao;
+    }
+    public SoPhuc(SoPhuc t2)
+    {
+        this.thuc = t2.thuc;
+        this.ao = t2.ao;
+    }
+    public void Nhap()
+    {
+        Console.Write("Nhap phan thuc:"); thuc = double.Parse(Console.ReadLine());
+        Console.Write("Nhap phan ao:"); ao = double.Parse(Console.ReadLine());
+    }
+    public void Hien()
+    {
+        string kq = thuc + (ao > 0 ? "+" : "") + ao + "*i";
+        Console.WriteLine(kq);
+    }
+    public static SoPhuc operator +(SoPhuc t1, SoPhuc t2)
+    {
+        SoPhuc tmp = new SoPhuc();
+        tmp.thuc = t1.thuc + t2.thuc;
+        tmp.ao = t1.ao + t2.ao;
+        return tmp;
+    }
+    public SoPhuc Tong(SoPhuc t2)
+    {
+        SoPhuc tmp = new SoPhuc();
+        tmp.thuc = this.thuc + t2.thuc;
+        tmp.ao = this.ao + t2.ao;
+        return tmp;
+    }
+}
+//Bai5
+class Matran
+{
+    private int sh, sc;
+    private int[,] a;
+    public Matran()
+    {
+        sh = sc = 0;
+        a = new int[sh, sc];
+    }
+    public Matran(int sh, int sc)
+    {
+        this.sh = sh;
+        this.sc = sc;
+        a = new int[sh, sc];
+    }
+    public Matran(Matran t2)
+    {
+        this.sh = t2.sh;
+        this.sc = t2.sc;
+        this.a = new int[sh, sc];
+        for (int i = 0; i < sh; i++)
+            for (int j = 0; j < sc; j++)
+                this.a[i, j] = t2.a[i, j];
+    }
+    public void print()
+    {
+        for (int i = 0; i < sh; i++)
+        {
+            for (int j = 0; j < sc; j++)
+                Console.Write("{0}\t", a[i, j]);
+            Console.WriteLine();
+        }
+    }
+    public void Nhap()
+    {
+        for (int i = 0; i < sh; i++)
+            for (int j = 0; j < sc; j++)
+            {
+                Console.Write("a[{0},{1}]=", i, j);
+                a[i, j] = int.Parse(Console.ReadLine());
+            }
+    }
+    public Matran Tong(Matran t2)
+    {
+        if (this.sh == t2.sh && this.sc == t2.sc)
+        {
+            Matran t = new Matran(this.sh, this.sc);
+            for (int i = 0; i < sh; i++)
+                for (int j = 0; j < sc; j++)
+                    t.a[i, j] = this.a[i, j] + t2.a[i, j];
+            return t;
+        }
+        else return null;
+    }
+    public Matran Hieu(Matran t2)
+    {
+        if (this.sh == t2.sh && this.sc == t2.sc)
+        {
+            Matran t1 = new Matran(this.sh, this.sc);
+            for (int i = 0; i < sh; i++)
+                for (int j = 0; j < sc; j++)
+                    t1.a[i, j] = this.a[i, j] - t2.a[i, j];
+            return t1;
+        }
+        else return null;
+    }
+}
